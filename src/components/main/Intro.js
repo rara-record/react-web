db48cbbb133b0ed8be43e40b409df9eca2bc9819
bora <a52447879@gamil.com>
2021-12-30 20:52:47 +0900

    인트로 수정

--- src/components/main/Intro.js
+++ src/components/main/Intro.js
@@ -7,31 +7,40 @@ function Intro() {
     <>
       <section className="intro">
         <div className="inner">
-          <h1 className="visually-hidden">INTRO</h1>
-          <div className="intro__left">
+          <div className="intro__title">
+            <h1>service</h1>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit,{' '}
+              <br></br>sed do eiusmod tempor incididunt ut labore et dolore
+              magna aliqua ut.
+            </p>
+          </div>
+
+          <div className="icon__wrpper">
             {IntroData.map((data, index) => {
               return (
-                <article key={index}>
-                  <h2>{data.title}</h2>
-                  <div className="wrap">
-                    <span>{data.number}</span>
-                    <p>{data.description}</p>
+                <article className="icon__list" key={index}>
+                  <div className="icon__front">
+                    <figure className="icon__media">
+                      <img
+                        src={`${path}/img/${data.icon}.png`}
+                        alt={data.alt1}
+                      />
+                    </figure>
+                    <div className="icon__text">
+                      <h2>{data.title}</h2>
+                      <p>{data.description}</p>
+                    </div>
+                  </div>
+                  <div className="icon__back">
+                    <img
+                      src={`${path}/img/${data.image}.jpg`}
+                      alt={data.alt2}
+                    />
                   </div>
                 </article>
               )
             })}
-          </div>
-          <div className="intro__right">
-            <h3>
-              See how we<br></br>works
-            </h3>
-            <p>
-              Let's see how we are woking. Discover the best<br></br> interior
-              design for your sweet home.
-            </p>
-            <figure>
-              <img src={`${path}/img/intro.jpg`} alt="" />
-            </figure>
           </div>
         </div>
       </section>
--- src/data/IntroData.js
+++ src/data/IntroData.js
@@ -1,20 +1,40 @@
 export const IntroData = [
   {
-    title: 'Planing',
-    number: 'step 01',
+    title: 'PROPERTY SIZE',
     description:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit amet eos perferendis.',
+      'Lorem ipsum dolor sit amet, conse ctet uradipiscing amet nus.',
+    icon: 'intro-icon-01',
+    image: 'intro-image-01',
+    alt1: 'INTRO ICON',
+    alt2: 'INTRO IMAGE',
   },
+
   {
-    title: 'Estimaing',
-    number: 'step 02',
+    title: 'STRUCTURE',
     description:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit amet eos perferendis.',
+      'Lorem ipsum dolor sit amet, conse ctet uradipiscing amet nus.',
+    icon: 'intro-icon-02',
+    image: 'intro-image-02',
+    alt1: 'INTRO ICON',
+    alt2: 'INTRO IMAGE',
   },
+
   {
-    title: 'Building',
-    number: 'step 03',
+    title: 'ACCOMMODATION',
     description:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit amet eos perferendis.',
+      'Lorem ipsum dolor sit amet, conse ctet uradipiscing amet nus.',
+    icon: 'intro-icon-03',
+    image: 'intro-image-03',
+    alt1: 'INTRO ICON',
+    alt2: 'INTRO IMAGE',
+  },
+
+  {
+    title: 'HEATING',
+    description:
+      'Lorem ipsum dolor sit amet, conse ctet uradipiscing amet nus.',
+    icon: 'intro-icon-04',
+    image: 'intro-image-04',
+    alt1: 'INTRO ICON',
   },
 ]
